1353181e834a029ae53af7ff74f7e4148a93cd4c
Victoria Ferreira <victoria.senac.13@gmail.com>
2024-12-02 21:20:54 -0300

    Revert "Merge branch 'main' of https://github.com/victoriafe-sa/Projeto-ALendaVista"
    This reverts commit ee5ad790f6b972733412931e5ae70a9dc2048963, reversing
    changes made to d8d433d8ba371d20fef94d62e17f973273098a51.

--- script/modal_login.js
+++ script/modal_login.js
@@ -1,39 +1,23 @@
 // Obter o modal
 var modal = document.getElementById("loginModal");
 
-// Obter o botão que abre o login
-var btn = document.getElementById("loginBtn")
-var btn1 = document.getElementById("loginBut1")
-var btn2 = document.getElementById("loginBut2")
-var btn3 = document.getElementById("loginBut3")
-var btn4 = document.getElementById("loginBut4");
+// Obter o botão que abre o modal
+var btn = document.getElementById("loginBtn");
 
 // Obter o botão de fechar
 var closeBtn = document.getElementById("closeBtn");
 
-// Quando clica no botão, abre o modal)
+// Quando o usuário clica no botão, abre o modal (se você tiver um botão com id 'loginBtn')
 btn.onclick = function() {
     modal.style.display = "block";
 }
-btn1.onclick = function() {
-    modal.style.display = "block";
-}
-btn2.onclick = function() {
-    modal.style.display = "block";
-}
-btn3.onclick = function() {
-    modal.style.display = "block";
-}
-btn4.onclick = function() {
-    modal.style.display = "block";
-}
 
-// Quando o usuário clica no botão de fechar, fecha
+// Quando o usuário clica no botão de fechar, fecha o modal
 closeBtn.onclick = function() {
     modal.style.display = "none";
 }
 
-// Quando o usuário clica fora do modal, fecha
+// Quando o usuário clica fora do modal, fecha o modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
